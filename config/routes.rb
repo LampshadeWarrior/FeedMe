8816d742e3cf03880ed2a567099b6f98bfeecb85
Iann Singer <itpsinger@gmail.com>
2018-04-05 19:29:28 +0000

    Create rake action for rss feed sync

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'entries/index'
+
+  get 'entries/show'
+
   root 'feeds#index'
   resources :feeds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
