554632162527f6ac8e1453c4b467194fd7d3e806
Iann Singer <itpsinger@gmail.com>
2018-04-05 19:42:06 +0000

    Set up entries controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'feeds#index'
-  resources :feeds
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :feeds do
+    member do
+      resources :entries, only: [:index, :show]
+    end
+  end
 end
