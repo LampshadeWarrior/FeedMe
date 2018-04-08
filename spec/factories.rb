f4f15babf469ef9572d141baeca750d1475bfa96
Iann Singer <itpsinger@gmail.com>
2018-04-08 20:47:59 +0000

    Define factorybot for feed and create tests for show and index view

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+
+  factory :feed do
+    sequence :name do |n|
+      "Fake Feed #{n}"
+    end
+    sequence :url do
+      "https://rss.simplecast.com/podcasts/2389/rss"
+    end
+    sequence :description do
+      "Fake description #{n}"
+    end
+  end
+end
