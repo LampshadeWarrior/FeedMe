bfac03962df8207b2d476363f62512e071e07abc
Iann Singer <itpsinger@gmail.com>
2018-05-31 17:20:07 +0000

    Validation tests for entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,8 @@
 class Entry < ApplicationRecord
-  belongs_to :feed 
+  belongs_to :feed
+  
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :content, presence: true
+  validates :feed_id, presence: true
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -4,11 +4,45 @@ RSpec.describe Entry, type: :model do
 
   subject { described_class.new}
 
-  it "is valid with valid attributes"
+  it "is valid with valid attributes" do
     user = FactoryBot.create(:user)
-    feed = FactoryBot.create(:feed)
-  it "is not valid without a title"
-  it "is not valid without content"
-  it "is not valid without an author"
-  it "is not valid without a feed_id"
+    feed = FactoryBot.create(:feed, :user_id => user.id)
+    entry = FactoryBot.create(:entry, :feed_id => feed.id)
+    expect(entry).to be_valid
+  end
+
+  it "is not valid without a title" do
+    user = FactoryBot.create(:user)
+    feed = FactoryBot.create(:feed, :user_id => user.id)
+    subject.content = "Whatever"
+    subject.author = "Whomever"
+    subject.feed_id = feed.id
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without content" do
+    user = FactoryBot.create(:user)
+    feed = FactoryBot.create(:feed, :user_id => user.id)
+    subject.author = "Whomever"
+    subject.feed_id = feed.id
+    subject.title = "Whatever"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an author" do
+    user = FactoryBot.create(:user)
+    feed = FactoryBot.create(:feed, :user_id => user.id)
+    subject.feed_id = feed.id
+    subject.title = "Whatever"
+    subject.content = "Whatever"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a feed_id" do
+    subject.title = "Whatever"
+    subject.content = "Whatever"
+    subject.author = "Whomever"
+    expect(subject).to_not be_valid
+  end
+
 end
