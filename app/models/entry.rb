b3cd318d18e407367cb660f643beaca8b0340277
Iann Singer <itpsinger@gmail.com>
2018-06-22 18:16:59 +0000

    Allow entries to be added without an author

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :feed
-  
+
   validates :title, presence: true
-  validates :author, presence: true
   validates :content, presence: true
   validates :feed_id, presence: true
 end
